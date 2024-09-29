27a7467989a3eadcda45f7d50f3743a70a735f90
lilYummy228 <142146352+lilYummy228@users.noreply.github.com>
2024-09-29 11:11:02 +0500

    Добавьте файлы проекта.

---
+++ CleanCode&CodeStyle1/Program.cs
@@ -0,0 +1,11 @@
+﻿internal class Program
+{
+    public static int TryFindElement(int[] array, int element)
+    {
+        for (int i = 0; i < array.Length; i++)
+            if (array[i] == element)
+                return i;
+
+        return -1;
+    }
+}
